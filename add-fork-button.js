2562d0bd384ac87858513d6554716afaf0e50b9f
PGMY <pgmyjp@gmail.com>
2014-12-13 14:00:27 +0900

    フォークボタン

--- add-fork-button.js
+++ add-fork-button.js
@@ -9,7 +9,13 @@ $(function(){
   var kekka1 = window.location.href.split("\/");
   var kekka2 = kekka1[kekka1.length-1].split("#");
   var id = kekka2[0];
-  alert(id);
+  // alert(id);
   var param = "?fork=true&items="+id;
-  $('.itemsShowHeaderStock').append('<a class="btn btn-primary stock-button btn-block js-stock-btn" data-position="top" href="http://qiita.com/drafts/new'+param+'" title="Stock">フォークする</a>');
+  var forkLink = $('<a class="btn btn-primary btn-block" data-position="top" href="http://qiita.com/drafts/new'+param+'" title="Fork">フォークする</a>');
+  forkLink.css("background-color","#f0ad4e");
+  forkLink.css("border-color","#e29a42");
+  $('.itemsShowHeaderStock').append(forkLink);
 });
+
+
+// style="background-color:#f0ad4e;"
