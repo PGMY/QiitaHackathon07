bbffc04bceb7b2aa6d62fb908f5ef7da473ffe9a
PGMY <pgmyjp@gmail.com>
2014-12-13 13:02:23 +0900

    フォークするボタン追加

--- add-fork-button.js
+++ add-fork-button.js
@@ -5,5 +5,6 @@ window.MyEmbededProgram = {
 };
 
 $(function(){
-  // alert("Hello World!");
+  // alert(window.location.href);
+  $('.itemsShowHeaderStock').append('<a class="btn btn-primary stock-button btn-block js-stock-btn" data-position="top" href="javascript:void(0)" title="Stock">フォークする</a>');
 });
