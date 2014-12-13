65c68314ab1b816b56d51266fde3a25a9a206198
PGMY <pgmyjp@gmail.com>
2014-12-13 13:22:56 +0900

    フォークボタン パラメータつけて投稿ページに遷移させる

--- add-fork-button.js
+++ add-fork-button.js
@@ -6,6 +6,10 @@ window.MyEmbededProgram = {
 
 $(function(){
   // alert(window.location.href);
-  var param = 
-  $('.itemsShowHeaderStock').append('<a class="btn btn-primary stock-button btn-block js-stock-btn" data-position="top" href="'+param+'" title="Stock">フォークする</a>');
+  var kekka1 = window.location.href.split("\/");
+  var kekka2 = kekka1[kekka1.length-1].split("#");
+  var id = kekka2[0];
+  alert(id);
+  var param = "?fork=true&items="+id;
+  $('.itemsShowHeaderStock').append('<a class="btn btn-primary stock-button btn-block js-stock-btn" data-position="top" href="http://qiita.com/drafts/new'+param+'" title="Stock">フォークする</a>');
 });
